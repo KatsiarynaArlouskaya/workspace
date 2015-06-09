762f301d35421e30005d8f47cfd85a9c65888ad6
Katsiaryna Arlouskaya <Katsiaryna_Arlouskaya@epam.com>
2015-06-09 14:35:50 +0300

    Update task03, lesson01

--- AT0003_Lesson01Java/src/com/epam/lesson01/task03/Arlouskaya/main.java
+++ AT0003_Lesson01Java/src/com/epam/lesson01/task03/Arlouskaya/main.java
@@ -4,9 +4,28 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class main {
+	static double a, b;
 
 	public static void main(String[] args) {
-		double a = 0, b = 0;
+		Boolean isNumbers = getDate();
+		if (isNumbers) {
+			getPerimetr();
+			getArea();
+		}
+
+	}
+
+	private static void getPerimetr() {
+		double perimeter = a + b + Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+		System.out.println("Perimeter=" + perimeter);
+	}
+
+	private static void getArea() {
+		double area = a * b / 2;
+		System.out.println("Area=" + area);
+	}
+
+	private static Boolean getDate() {
 		Scanner in = new Scanner(System.in);
 		Boolean isNumbers = true;
 		try {
@@ -19,18 +38,11 @@ public class main {
 			isNumbers = false;
 		}
 		in.close();
-		if (isNumbers) {
-			if ((a > 0) && (b > 0)) {
-				double perimeter = a + b
-						+ Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-				System.out.println("Perimeter=" + perimeter);
-				double area = a * b / 2;
-				System.out.println("Area=" + area);
-			} else {
-				System.err.print("Inputs are not a positive number");
-			}
+		if ((a <= 0) || (b <= 0)) {
+			isNumbers = false;
+			System.err.print("Inputs are not a positive number");
 		}
-
+		return isNumbers;
 	}
 
 }
